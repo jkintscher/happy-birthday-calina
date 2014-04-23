e90a4b5198b8d3fdf80914971349ceb00cb46d8e
Joschka <joschka@kintscher.me>
2014-04-23 17:10:50 +0200

    Tweak the story, change a few blanks

--- assets/stories.js
+++ assets/stories.js
@@ -1,13 +1,8 @@
-
-// 1: Example Mad Lips
-// "{{exclamation:Stop}}! he said {{adverb:loudly}} as he jumped into his " +
-// "convertible {{noun:car}} and drove off with his {{adjective:aroused}} wife."
-
 window.STORY =
   "Like every {{time of day:morning}}, Calina gets to the {{place:office}} early." +
   "<br />" +
   "<br />" +
-  "Nobody else is in the Harvest office yet and so she decides to have {{beverage:tea}} " +
+  "Nobody else is in the Harvest office yet and so she decides to have a {{beverage:tea}} " +
   "before starting her first shift—without {{ingredient:sugar}}, of course, as that doesn’t " +
   "go well with a sugar-free diet." +
   "<br />" +
@@ -15,10 +10,10 @@ window.STORY =
   "<br />" +
   "<br />" +
   "While walking back to her {{location:desk}} through the empty office she thinks about " +
-  "that latest short story by George Saunders. “What a great {{occupation:writer}}”, she says " +
+  "that latest {{noun:short story}} by George Saunders. “What a great {{occupation:writer}}”, she says " +
   "to herself. Calina is {{adjective (mood):delighted}} as usual and looking forward to another {{adjective:fun}} " +
-  "and {{adjective:exciting}} day at Harvest, {{verb (-ing):helping out}} customers and becoming better at " +
-  "understanding Futurist poems at the same time. “This is something racing " +
+  "and {{another adjective:exciting}} day at Harvest, {{verb (-ing):helping out}} customers and becoming better at " +
+  "understanding Futurist {{noun:poems}} at the same time. “This is something racing " +
   "Japanese muscle cars could never have taught me”, " +
   "she realizes, when suddenly the front door opens with a loud {{sound:noise}}." +
   "<br />" +
@@ -32,10 +27,10 @@ window.STORY =
   "“It’s gone! I have lost my phone!”. Christopher laughs." +
   "<br />" +
   "<br />" +
-  "A few minutes pass and Calina becomes conscious again, woken up by the " +
+  "{{timeframe:A few minutes}} pass and Calina becomes conscious again, woken up by the " +
   "distinct sound of Shawn complaning about a {{noun:cookie}} in another corner of the " +
-  "{{place:office}}. “What happened?”, she asks Christopher, who is jamming out to his " +
-  "favorite tunes at his {{location:desk}} across from her. “What happened?”. Also her " +
+  "office. “What happened?”, she asks Christopher, who is jamming out to his " +
+  "favorite tunes at his desk across from her. “What happened?”. Also her " +
   "second question goes unanswered when a familar thought comes to mind: " +
   "“Sometimes I wish I would be working from {{location:within a whale’s heart}}. No drama, " +
   "just being at piece.”, she thinks. " +
@@ -46,7 +41,8 @@ window.STORY =
   "It takes her to a {{appearance:beautiful}} website that, after prompting her to input " +
   "{{count:several}} words, presents her with a precise write-up of her morning. " +
   "Reading it with great {{noun (mood):pleasure}} all the way to the end makes her understand " +
-  "the reasons for all of the weird events of her morning: It’s her {{occasion:birthday}}!" +
+  "the reasons for all of the {{adjective:weird}} events of her morning: " +
+  "It’s her {{occasion:birthday}}!" +
   "<br />" +
   "<br />" +
   "Happy Birthday, Calina!";
