efce836143251d538c0cee65629b34885165111d
Joschka <joschka@kintscher.me>
2014-04-23 18:19:36 +0200

    Escape unicode characters

--- assets/stories.js
+++ assets/stories.js
@@ -3,37 +3,37 @@ window.STORY =
   "<br />" +
   "<br />" +
   "Nobody else is in the Harvest office yet and so she decides to have a {{beverage:tea}} " +
-  "before starting her first shift—without {{ingredient:sugar}}, of course, as that doesn’t " +
+  "before starting her first shift&mdash;without {{ingredient:sugar}}, of course, as that doesn&rsquo;t " +
   "go well with a sugar-free diet." +
   "<br />" +
   "The kettle boils, and the first round of tickets awaits." +
   "<br />" +
   "<br />" +
   "While walking back to her {{location:desk}} through the empty office she thinks about " +
-  "that latest {{noun:short story}} by George Saunders. “What a great {{occupation:writer}}”, she says " +
+  "that latest {{noun:short story}} by George Saunders. &ldquo;What a great {{occupation:writer}}&rdquo;, she says " +
   "to herself. Calina is {{adjective (mood):delighted}} as usual and looking forward to another {{adjective:fun}} " +
   "and {{another adjective:exciting}} day at Harvest, {{verb (-ing):helping out}} customers and becoming better at " +
-  "understanding Futurist {{noun:poems}} at the same time. “This is something racing " +
-  "Japanese muscle cars could never have taught me”, " +
+  "understanding Futurist {{noun:poems}} at the same time. &ldquo;This is something racing " +
+  "Japanese muscle cars could never have taught me&rdquo;, " +
   "she realizes, when suddenly the front door opens with a loud {{sound:noise}}." +
   "<br />" +
   "<br />" +
-  "Quickly she looks back over her shoulder: It’s Christopher walking in with " +
-  "Pickles, his girlfriend’s {{animal:dog}}. Being the {{adjective:loving}} person Calina is she greets " +
+  "Quickly she looks back over her shoulder: It&rsquo;s Christopher walking in with " +
+  "Pickles, his girlfriend&rsquo;s {{animal:dog}}. Being the {{adjective:loving}} person Calina is she greets " +
   "Pickles {{adverb:heartwarmingly}}. After a while of playing with it she reaches for her " +
   "phone in order to share their {{adjective:cute}} picture with the world." +
   "<br />" +
-  "Suddenly, a loud scream breaks the idyllic scene: “Oh no!”, she shrieks. " +
-  "“It’s gone! I have lost my phone!”. Christopher laughs." +
+  "Suddenly, a loud scream breaks the idyllic scene: &ldquo;Oh no!&rdquo;, she shrieks. " +
+  "&ldquo;It&rsquo;s gone! I have lost my phone!&rdquo;. Christopher laughs." +
   "<br />" +
   "<br />" +
   "{{timeframe:A few minutes}} pass and Calina becomes conscious again, woken up by the " +
   "distinct sound of Shawn complaning about a {{noun:cookie}} in another corner of the " +
-  "office. “What happened?”, she asks Christopher, who is jamming out to his " +
-  "favorite tunes at his desk across from her. “What happened?”. Also her " +
+  "office. &ldquo;What happened?&rdquo;, she asks Christopher, who is jamming out to his " +
+  "favorite tunes at his desk across from her. &ldquo;What happened?&rdquo;. Also her " +
   "second question goes unanswered when a familar thought comes to mind: " +
-  "“Sometimes I wish I would be working from {{location:within a whale’s heart}}. No drama, " +
-  "just being at piece.”, she thinks. " +
+  "&ldquo;Sometimes I wish I would be working from {{location:within a whale&rsquo;s heart}}. No drama, " +
+  "just being at piece.&rdquo;, she thinks. " +
   "<br />" +
   "<br />" +
   "Finally going back to answering all the {{adjective:important}} questions about {{activity:time " +
@@ -42,7 +42,7 @@ window.STORY =
   "{{count:several}} words, presents her with a precise write-up of her morning. " +
   "Reading it with great {{noun (mood):pleasure}} all the way to the end makes her understand " +
   "the reasons for all of the {{adjective:weird}} events of her morning: " +
-  "It’s her {{occasion:birthday}}!" +
+  "It&rsquo;s her {{occasion:birthday}}!" +
   "<br />" +
   "<br />" +
   "Happy Birthday, Calina!";
