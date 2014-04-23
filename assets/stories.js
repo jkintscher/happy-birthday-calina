ed796b56f8a8e20ff6e632c1a61f8d8ac506e062
Joschka <joschka@kintscher.me>
2014-04-23 08:29:25 +0200

    First iteration of blanks

--- assets/stories.js
+++ assets/stories.js
@@ -4,49 +4,49 @@
 // "convertible {{noun:car}} and drove off with his {{adjective:aroused}} wife."
 
 window.STORY =
-  "Like every morning, Calina gets to work early." +
+  "Like every {{time of day:morning}}, Calina gets to the {{place:office}} early." +
   "<br />" +
   "<br />" +
-  "Nobody else is in the Harvest office yet and so she decides to make tea " +
-  "before starting her first shift—without sugar, of course, as that doesn’t " +
+  "Nobody else is in the Harvest office yet and so she decides to have {{beverage:tea}} " +
+  "before starting her first shift—without {{ingredient:sugar}}, of course, as that doesn’t " +
   "go well with a sugar-free diet." +
   "<br />" +
   "The kettle boils, and the first round of tickets awaits." +
   "<br />" +
   "<br />" +
-  "While walking back to her desk through the empty office she thinks about " +
-  "that latest short story by George Saunders. “What a great writer”, she says " +
-  "to herself. Calina is delighted as usual and looking forward to another fun " +
-  "and exciting day at Harvest, helping out customers and becoming better at " +
+  "While walking back to her {{location:desk}} through the empty office she thinks about " +
+  "that latest short story by George Saunders. “What a great {{occupation:writer}}”, she says " +
+  "to herself. Calina is {{adjective (mood):delighted}} as usual and looking forward to another {{adjective:fun}} " +
+  "and {{adjective:exciting}} day at Harvest, {{verb (-ing):helping out}} customers and becoming better at " +
   "understanding Futurist poems at the same time. “This is something racing " +
   "Japanese muscle cars could never have taught me”, " +
-  "she realizes, when suddenly the front door opens with a loud noise." +
+  "she realizes, when suddenly the front door opens with a loud {{sound:noise}}." +
   "<br />" +
   "<br />" +
   "Quickly she looks back over her shoulder: It’s Christopher walking in with " +
-  "Pickles, his girlfriend’s dog. Being the loving person Calina is she greets " +
-  "Pickles heartwarmingly. After a while of playing with it she reaches for her " +
-  "phone in order to share their cute picture with the world." +
+  "Pickles, his girlfriend’s {{animal:dog}}. Being the {{adjective:loving}} person Calina is she greets " +
+  "Pickles {{adverb:heartwarmingly}}. After a while of playing with it she reaches for her " +
+  "phone in order to share their {{adjective:cute}} picture with the world." +
   "<br />" +
   "Suddenly, a loud scream breaks the idyllic scene: “Oh no!”, she shrieks. " +
   "“It’s gone! I have lost my phone!”. Christopher laughs." +
   "<br />" +
   "<br />" +
   "A few minutes pass and Calina becomes conscious again, woken up by the " +
-  "distinct sound of Shawn complaning about a cookie in another corner of the " +
-  "office. “What happened?”, she asks Christopher, who is jamming out to his " +
-  "favorite tunes at his desk across from her. “What happened?”. Also her " +
+  "distinct sound of Shawn complaning about a {{noun:cookie}} in another corner of the " +
+  "{{place:office}}. “What happened?”, she asks Christopher, who is jamming out to his " +
+  "favorite tunes at his {{location:desk}} across from her. “What happened?”. Also her " +
   "second question goes unanswered when a familar thought comes to mind: " +
-  "“Sometimes I wish I would be working from within a whale’s heart. No drama, " +
+  "“Sometimes I wish I would be working from {{location:within a whale’s heart}}. No drama, " +
   "just being at piece.”, she thinks. " +
   "<br />" +
   "<br />" +
-  "Finally going back to answering all the important questions about time " +
-  "tracking and invoicing she stumbles across a strangely looking link. " +
-  "It takes her to a beautiful website that, after prompting her to input " +
-  "several words, presents her with a precise write-up of her morning. " +
-  "Reading it with great pleasure all the way to the end makes her understand " +
-  "the reasons for all of the weird events of her morning: It’s her birthday!" +
+  "Finally going back to answering all the {{adjective:important}} questions about {{activity:time " +
+  "tracking}} and {{another activity:invoicing}} she stumbles across a strangely looking link. " +
+  "It takes her to a {{appearance:beautiful}} website that, after prompting her to input " +
+  "{{count:several}} words, presents her with a precise write-up of her morning. " +
+  "Reading it with great {{noun (mood):pleasure}} all the way to the end makes her understand " +
+  "the reasons for all of the weird events of her morning: It’s her {{occasion:birthday}}!" +
   "<br />" +
   "<br />" +
   "Happy Birthday, Calina!";
